f7f5ad64745a7bd51372b383470b2cf443c2bc95
vaspupkinko <42377637+vaspupkinko@users.noreply.github.com>
2019-01-29 21:28:03 +0200

    Update 2.php

--- 2.php
+++ 2.php
@@ -10,9 +10,7 @@ $green  = "\e[92m";
 $red    = "\e[91m";
 $yellow = "\e[93m";
 $blue   = "\e[36m";
-$i = 0;
-for ($i <= 100){
-  $i++;
+for ($d = 1; $d <= 4; $d++) { 
   echo "...................\n";
   sleep(0.5);
   echo "...\m";
