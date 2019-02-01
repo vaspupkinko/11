419fbc78438b4eb2a6b225cde94afa7935776136
vaspupkinko <42377637+vaspupkinko@users.noreply.github.com>
2019-02-02 00:02:42 +0200

    Update 2.php

--- 2.php
+++ 2.php
@@ -11,6 +11,30 @@ $g = "\e[92m";
 $r = "\e[91m";
 $y = "\e[93m";
 $b = "\e[36m";
+$empt = "   ";
+$gr = "OOO";
+$next_line = "\n";
+for ($x=0; $x<10; $x++){
+   for ($i=0; $i<47; $i++){
+      echo $empt;
+      echo $next_lime;
+   }
+   for ($a=0; $a<47; $a++){
+      echo $empt;
+      echo $next_line;
+   }
+   for ($b=0; $b<47; $b++){
+      echo $empt;
+      echo $next_line;
+   }
+   for ($c=0; $c<47; $c++){
+      echo $gr;
+      echo $next_line;
+   }
+}
+echo $next_line;
+echo $next_line;
+echo $next_line;
    echo "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n";
    echo "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n";
 echo "$red====================================>".$green."\n";
